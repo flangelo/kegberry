2050ff6ad984638fd302656a248a4a500c2a2034
Frode Langelo <frode@langelo.net>
2026-06-16 12:29:39 -0700

    Modernise db/ image to populate MariaDB timezone tables on init
    Rebuild on mariadb:10.11 (official image) instead of the old Alpine
    base. Installs tzdata and uses /docker-entrypoint-initdb.d/ to run
    mysql_tzinfo_to_sql on first init, fixing the Django
    "Database returned an invalid datetime value" error on /sessions/.
    
    Drops the obsolete entrypoint.sh with InnoDB Barracuda flags removed
    in MariaDB 10.3+.

--- db/entrypoint.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-/scripts/init_tzdata.sh & /scripts/run.sh --innodb_file_format=Barracuda --innodb_default_row_format=DYNAMIC --innodb_large_prefix=ON
--- db/init_tzdata.sh
+++ db/init_tzdata.sh
@@ -1,22 +1,3 @@
-#!/bin/sh
-# Wait to be sure that MySql is up
-while ! /usr/bin/mysql -u root -e "use mysql;"
-do
-    sleep 2
-done
-
-echo "APPLY SYSTEM TIMEZONES START"
-
-query="select count(*) from time_zone"
-
-count=`/usr/bin/mysql -u root --skip-column-names mysql << eof
-$query
-eof`
-
-if [ $count -eq 0 ]
-then
-    sh -c "mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql -u root mysql"
-    echo "APPLY SYSTEM TIMEZONES COMPLETE"
-else
-    echo "SYSTEM TIMEZONES ALREAY POPULATED"
-fi
+#!/bin/bash
+# Populate MySQL timezone tables so Django's CONVERT_TZ() works correctly.
+mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql -u root mysql
